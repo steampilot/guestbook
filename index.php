61e52a1032f5a0187492cc2141267b849ff8cf17
Steampilot <github@steampilot.ch>
2014-09-10 17:01:07 +0200

    Improved sql query string. Fixed crappy implementation of for each in index.php

--- index.php
+++ index.php
@@ -5,28 +5,35 @@
  * Date: 04.09.14
  * Time: 10:42
  */
-require_once __DIR__.'/app/autoload.php';
 
+require_once __DIR__.'/app/autoload.php';
+echo(' <meta charset="UTF-8"> ');
 /**
  * Aguirre the database object
  */
+\Config::init();
 $db = App::getDb();
 var_dump($db);
+
+
 
 /**
  * yield the content of the users table
  */
-$sql = 'SELECT * FROM spgb.users;';
-foreach ($db->query($sql) as $row) {
-	var_dump($row);
+$sql = 'SELECT * FROM users;';
+var_dump($sql);
+$rows = $db->query($sql);
+foreach ( $rows as $row) {
+var_dump($row);
 }
 
 /**
  * yield the content of the posts table
  */
-$sql = 'SELECT * FROM spgb.posts;';
-foreach ($db->query($sql) as $row) {
- var_dump($row);
+$sql = 'SELECT * FROM posts;';
+$rows = $db->query($sql);
+foreach ( $rows as $row) {
+	var_dump($row);
 }
 
 
