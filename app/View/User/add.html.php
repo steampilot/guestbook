77b9217ec38ba1a653d939f13ceb6b0a92023b0e
Steampilot <github@steampilot.ch>
2014-10-17 20:03:25 +0200

    Implemented user registration from scratch

--- app/View/User/add.html.php
+++ app/View/User/add.html.php
@@ -9,9 +9,17 @@
 
 $session_user_id = 2;
 $today = $app['today'];
-$btnUrl = __BASE_URL__.'User/index';
+$btnUrl = __BASE_URL__.'Post/index';
 $btnText = 'Back to the List';
-$submitUrl = __BASE_URL__.'User/add'
+$submitUrl = __BASE_URL__.'User/add';
+$name = '';
+$email = '';
+$alert = false;
+if(isset($_POST) && !empty($_POST)){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$alert = true;
+}
 ?>
 <main class="container">
 	<header class="jumbo-narrow col-lg-4">
@@ -19,7 +27,7 @@ $submitUrl = __BASE_URL__.'User/add'
 			Create New User
 		</h1>
 		<p>
-			Password will be set afterwards
+			You will be able to write to this message board.
 		</p>
 		<p>
 			<a href="<?php pu($btnUrl);?>" class="btn btn-primary btn-lg" role="button"><?php ph($btnText);?> &raquo;</a>
@@ -28,7 +36,7 @@ $submitUrl = __BASE_URL__.'User/add'
 	<article id='postView' class="col-lg-8">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h3>Write a Post</h3>
+				<h3>Your account settings</h3>
 			</div>
 			<div class="panel-body">
 				<form role="form" class="" accept-charset="UTF-8" action="<?php pu($submitUrl); ?>" method="post">
@@ -36,11 +44,17 @@ $submitUrl = __BASE_URL__.'User/add'
 					<input type="hidden" name="role" value="3">
 					<div class="form-group">
 						<label for="subject">Name</label>
-						<input type="text" class="form-control" name="name" id="name" placeholder="Human readable name">
+						<input type="text" class="form-control"
+						       name="name" id="name"
+						       value="<?php ph($name); ?>"
+						       placeholder="Your Name">
 					</div>
 					<div class="form-group">
 						<label for="subject">Email</label>
-						<input type="email" class="form-control" name="email" id="email" placeholder="you@email.com">
+						<input type="email" class="form-control"
+						       name="email" id="email"
+						       value="<?php ph($email); ?>"
+						       placeholder="you@email.com">
 					</div>
 					<div class="form-group">
 						<label for="password">Password</label>
@@ -50,22 +64,18 @@ $submitUrl = __BASE_URL__.'User/add'
 						<label for="password-check">Retype Password</label>
 						<input class="form-control" type="password" name="password-check" id="password-check"  size="32">
 					</div>
-					<div class="form-inline">
-							<label class="radio-inline">
-								<input type="radio" name="role" id="role1" value="1"> Admin
-							</label>
-							<label class="radio-inline">
-								<input type="radio" name="role" id="role2" value="2"> Author
-							</label>
-							<label class="radio-inline">
-								<input type="radio" name="role" id="role3" value="3"> Guest
-							</label>
-					</div>
 					<button type="submit" id='submit' class="btn btn-default">Submit</button>
 				</form>
 			</div>
 			<div class="panel-footer">
-				created Today
+				<div class="row">
+					<div class="col-sm-4">
+						<small><strong>Created:</strong><?php ph($today); ?></small>
+					</div>
+					<div class="col-sm-4">
+						<small><strong>Modified:</strong><?php ph('Today'); ?></small>
+					</div>
+				</div>
 			</div>
 		</div>
 	</article>
