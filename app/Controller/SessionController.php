c8249c3e372eb2695c2e4627054853d052f4e710
Steampilot <github@steampilot.ch>
2014-10-17 19:50:20 +0200

    Implemented login and logout Fixed session destroy and continues display of alert message bug Improved alert messages

--- app/Controller/SessionController.php
+++ app/Controller/SessionController.php
@@ -8,16 +8,17 @@
 
 namespace Controller;
 
-
-
 class SessionController extends Controller{
+	/**
+	 * @var \Model\UserModel $model The Model
+	 */
+	protected $model = '';
 	public function __construct($params) {
 		$modelName = array('User','Users');
 		parent::__construct($params,$modelName);
 	}
 
 	public function login(){
-		echo'action = login method=post';
 		if($this->method === 'POST' && isset($_POST['login_email'])) {
 			$data = $this->model->getOneByEmail($_POST['login_email']);
 			if (empty($data)){
@@ -25,25 +26,47 @@ class SessionController extends Controller{
 				$_SESSION['active'] = false;
 				$this->setAlert('error', array(
 					'title' => 'ACCESS DENIED',
-					'text' => 'Come on you must do better than this to hack this site!'
+					'text' => 'No Email found witch means that we have no idea who you are and what you want.'
 				));
-				$this->redirect();
+				$this->redirect('Post', 'index');
 			} else {
-				$_SESSION['sessionUserId'] = $data['id'];
-				$_SESSION['active'] = true;
-				$this->setAlert('success', array(
-					'title' => 'ACCESS GRANTED',
-					'text' => 'You did remember your email and your password after all!'
-				));
-				$this->redirect();
+				if(password_verify($_POST['login_password'], $data['password'])){
+					$_SESSION['sessionUserId'] = $data['id'];
+					$_SESSION['active'] = true;
+					$this->setAlert('success', array(
+						'title' => 'ACCESS GRANTED',
+						'text' => 'You did remember your email and your password after all!'
+					));
+					$this->redirect('Post', 'index');
+				} else{
+					$this->setAlert('error', array(
+						'title' => 'ACCESS DENIED',
+						'text' => 'Wrong Password! No panic, just keep calm and think. Its somewhere lost in your
+						head.'
+					));
+					$_SESSION['sessionUserId'] = null;
+					$_SESSION['active'] = false;
+					$this->redirect('Post', 'index');
+				}
 			}
 		}
 
 
 	}
 	public function logout(){
-		session_destroy();
-	$this->redirect();
+		$this->destroySession();
+		$this->startSession();
+		$this->setAlert('success' ,array(
+			'title' => 'LOGGED OUT',
+			'text' => 'Despite of all the fun you had posting strange and crazy stuff on this message board. You
+			decided that it is enough for today. And You know what? We think that is a good idea. That\'s why we say
+			goodbye and as always: Thank you for posting!'
+		));
+
+		$this->redirect('Post', 'Index');
+	}
+	protected function comparePassword($hash,$password){
+
 	}
 
 } 
